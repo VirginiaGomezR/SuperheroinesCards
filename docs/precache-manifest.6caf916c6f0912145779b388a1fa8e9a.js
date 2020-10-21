c5fe244cefbdcaf33f5b5334bc82689c646bc898
Virginia <virginiagomezr@gmail.com>
2020-10-21 12:24:43 +0200

    docs folder

---
+++ docs/precache-manifest.6caf916c6f0912145779b388a1fa8e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6d920550bdda961d61975eb5c25bf5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc250272574dcada9fa8",
+    "url": "./static/css/main.044a5977.chunk.css"
+  },
+  {
+    "revision": "e8960f291c7e5c5223f2",
+    "url": "./static/js/2.e8d0c39d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e8d0c39d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc250272574dcada9fa8",
+    "url": "./static/js/main.f0f30373.chunk.js"
+  },
+  {
+    "revision": "14c91700c0a1af35a88a",
+    "url": "./static/js/runtime-main.578c5c65.js"
+  }
+]);
